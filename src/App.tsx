a9e4ccda9441d139499f8a98bbbe50137c8cf88f
anubhav-cmyk <anubhav@mantra.care>
2026-02-18 15:28:28 +0530

    Configure subpath hosting and CI/CD deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/feeling_fact">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
